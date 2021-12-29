89cc495227a80942d7cddf862047d73f84f6e954
Mansei <yarden835@gmail.com>
2021-12-29 11:55:18 +0200

    commit header

--- App.jsx
+++ App.jsx
@@ -1,27 +1,26 @@
-import { AppHome } from "./js/pages/app-home.jsx"
-import { AppAbout } from "./js/pages/app-about.jsx"
-import { EmailApp } from "./js/pages/EmailApp.jsx"
-import { KeepApp } from "./js/pages/KeepApp.jsx"
+import { AppHome } from './js/pages/app-home.jsx';
+import { AppAbout } from './js/pages/app-about.jsx';
+import { EmailApp } from './js/pages/EmailApp.jsx';
+import { KeepApp } from './js/pages/KeepApp.jsx';
+import { AppHeader } from './js/cmps/AppHeader.jsx';
 
+const Router = ReactRouterDOM.HashRouter;
+const { Route, Switch } = ReactRouterDOM;
 
-const Router = ReactRouterDOM.HashRouter
-const { Route, Switch } = ReactRouterDOM
-
-export function App(){
-    return (
-        <Router>
-            <section className="apps">
-                <main>
-                    <Switch>
-                        <Route component={AppAbout} path="/about"/>
-                        <Route component={KeepApp} path="/keep"/>
-                        <Route component={EmailApp} path="/email"/>
-                        <Route component={AppHome} path="/"/>
-                    </Switch>
-                </main>
-            </section>
-        </Router>
-    )
-
-
+export function App() {
+	return (
+		<Router>
+			<section className="apps">
+				<AppHeader />
+				<main>
+					<Switch>
+						<Route component={AppAbout} path="/about" />
+						<Route component={KeepApp} path="/keep" />
+						<Route component={EmailApp} path="/email" />
+						<Route component={AppHome} path="/" />
+					</Switch>
+				</main>
+			</section>
+		</Router>
+	);
 }
---
+++ js/cmps/AppHeader.jsx
@@ -0,0 +1,38 @@
+const { NavLink, withRouter } = ReactRouterDOM;
+
+class _AppHeader extends React.Component {
+	render() {
+		return (
+			<header className="app-header">
+				<div className="header-container">
+					<h1 onClick={() => this.props.history.push('/')}>Appsus</h1>
+					<nav className="app-nav">
+						<NavLink className="clean-link" activeClassName="my-activ" exact to="/">
+							Home
+						</NavLink>
+						<NavLink
+							className="clean-link"
+							activeClassName="my-active"
+							to="/keep">
+							Keep
+						</NavLink>
+						<NavLink
+							className="clean-link"
+							activeClassName="my-active"
+							to="/email">
+							eMail
+						</NavLink>
+						<NavLink
+							className="clean-link"
+							activeClassName="my-active"
+							to="/about">
+							About
+						</NavLink>
+					</nav>
+				</div>
+			</header>
+		);
+	}
+}
+
+export const AppHeader = withRouter(_AppHeader);
